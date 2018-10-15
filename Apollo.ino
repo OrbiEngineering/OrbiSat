cec502cc939a52d556089cc2a8bfcd9d7992959c
OrbiEngineering <44178625+OrbiEngineering@users.noreply.github.com>
2018-10-15 17:33:55 -0300

    Add files via upload

---
+++ Apollo.ino
@@ -0,0 +1,298 @@
+#include <VirtualWire.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+float d1;
+float d2;
+float d3;
+float d4;
+float d5;
+float d6;
+
+float p1;
+float p2;
+float p3;
+float p4;
+float p5;
+float p6;
+
+float alfa = 0;
+float alfa2 = 0;
+float beta = 0;
+float mat = 0;
+
+int analogPin1 = 1;
+int analogPin2 = 2;
+int analogPin3 = 3;
+int analogPin4 = 4;
+int analogPin5 = 5;
+int analogPin6 = 6;
+
+float maior = 0;
+float secmaior = 0;
+
+char Valor_CharMsg[4];
+int valRec;
+char recebido_RF_char[4];
+float valor;
+
+#define DHTPIN A0 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+
+{
+  Serial.begin(9600);
+
+  //RECEPTOR
+  vw_set_rx_pin(9);//Pino que estamos conectados
+  vw_setup(500);//Velocidade de bytes, quanto menor o valor, maior a precisão, porém, menor o alcance
+  vw_rx_start();
+
+  //TRANSMISSOR
+  vw_set_tx_pin(6);
+  vw_setup(500);
+
+  //SENSOR TEMPERATURA
+  dht.begin();
+}
+
+void loop()
+
+{
+  //Sensor de temperatura
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  //Painel Solar
+  maior = p1;
+  if (p2 > maior)  {
+    maior = p2;
+  }
+  if (p3 > maior)  {
+    maior = p3;
+  }
+  if (p4 > maior)  {
+    maior = p4;
+  }
+  if (p5 > maior)  {
+    maior = p5;
+  }
+  if (p6 > maior)  {
+    maior = p6;
+  }
+
+  d1 =  maior - p1;
+  d2 =  maior - p2;
+  d3 =  maior - p3;
+  d4 =  maior - p4;
+  d5 =  maior - p5;
+  d6 =  maior - p6;
+
+  secmaior = p1;
+  if (d2 < d1 && d2 > 0)  {
+    p2 = secmaior;
+  }
+  if (d3 < d2 && d3 > 0)  {
+    p3 = secmaior;
+  }
+  if (d4 < d3 && d4 > 0)  {
+    p4 = secmaior;
+  }
+  if (d5 < d4 && d5 > 0)  {
+    p5 = secmaior;
+  }
+  if (d6 < d5 && d6 > 0)  {
+    p6 = secmaior;
+  }
+
+  p1 = analogRead(analogPin1) * 0.00488759;
+  p2 = analogRead(analogPin2) * 0.00488759;
+  p3 = analogRead(analogPin3) * 0.00488759;;
+  p4 = analogRead(analogPin4) * 0.00488759;;
+  p5 = analogRead(analogPin5) * 0.00488759;;
+  p6 = analogRead(analogPin6) * 0.00488759;;
+
+  mat = maior / secmaior;
+  alfa = atan(mat);
+  alfa2 = alfa * 57.2958;
+  beta = 90 - alfa2;
+
+  delay(1000);
+
+  //RECEPTOR
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen))
+  {
+    int i;
+    for (i = 0; i < buflen; i++)
+    {
+      recebido_RF_char[i] = char(buf[i]); //Armazena os caracteres recebidos
+    }
+    recebido_RF_char[buflen] = '\0';
+    valRec = atoi(recebido_RF_char);
+
+    //TRANSMISSOR
+    //Estabilização
+    if (valRec == 1) {
+      
+      
+      valor = t;
+      dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+      delay(1000);
+      vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+      vw_wait_tx(); //Aguarda envio dos dados
+      Serial.println(Valor_CharMsg);
+      
+      //Envio Temperatura
+      if(valor = t) 
+      
+      { 
+       delay(1000);
+       valor = t;
+       dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+       delay(1000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+       Serial.println(Valor_CharMsg);
+    }}
+
+    //Estabilização
+    if (valRec == 2)
+
+    {
+      valor = h;
+      dtostrf (valor, 4, 2, Valor_CharMsg);//Converte valor para envio
+      delay(1000);
+      vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+      vw_wait_tx(); //Aguarda envio dos dados
+      Serial.println(Valor_CharMsg);
+      
+      delay(1000);
+      valor = h;
+      dtostrf (valor, 4, 2, Valor_CharMsg);//Converte valor para envio
+      delay(1000);
+      vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+      vw_wait_tx(); //Aguarda envio dos dados
+      Serial.println(Valor_CharMsg);
+     }
+    
+     //Envio Umidade     
+     if(valor = h) 
+     
+     {
+       delay(1000);
+       valor = h;
+       dtostrf (valor, 4, 2, Valor_CharMsg);//Converte valor para envio
+       delay(1000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+       Serial.println(Valor_CharMsg); 
+     }
+
+    //Estabilização
+    if (valRec == 3)
+
+    {
+      valor = p1;
+      dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+      delay(1000);
+      vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+      vw_wait_tx(); //Aguarda envio dos dados
+      
+      delay(1000);
+      valor = p1;
+      dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+      delay(1000);
+      vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+      vw_wait_tx(); //Aguarda envio dos dados
+      
+      delay(1000);
+      valor = p1;
+      dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+      delay(1000);
+      vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+      vw_wait_tx(); //Aguarda envio dos dados
+    
+    //Envio Painéis solares  
+    if(valor = p1) 
+     
+     {
+       delay(1000);
+       valor = p1;
+       dtostrf (valor, 4, 2, Valor_CharMsg);//Converte valor para envio
+       delay(2000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+       Serial.println(Valor_CharMsg); 
+
+      
+      
+       delay(1000);
+       valor = p2;
+       dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+       delay(2000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+
+       
+      
+       delay(1000);
+       valor = p3;
+       dtostrf (valor, 4, 2, Valor_CharMsg);//Converte valor para envio
+       delay(2000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+
+       
+       
+       delay(1000);
+       valor = p4;
+       dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+       delay(1000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+
+       
+       
+       delay(1000);
+       valor = p5;
+       dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+       delay(1000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+
+      
+      
+       delay(1000);
+       valor = p6;
+       dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+       delay(1000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+
+      
+      
+       delay(1000);
+       valor = alfa;
+       dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+       delay(1000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+
+       
+      
+       delay(1000);
+       valor = beta;
+       dtostrf (valor, 4, 2, Valor_CharMsg); //Converte valor para envio
+       delay(1000);
+       vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+       vw_wait_tx(); //Aguarda envio dos dados
+      }
+    }
+  }
+}
+
---
+++ Houston.ino
@@ -0,0 +1,406 @@
+#include <VirtualWire.h>
+
+int valor;
+char Valor_CharMsg[4];
+float valRec;
+char recebido_RF_char[4];
+char comando;
+
+void setup() {
+
+  Serial.begin(9600);
+
+  //TRANSMISSOR
+  vw_set_tx_pin(6);
+  vw_setup(500);
+
+  //RECEPTOR
+  vw_set_rx_pin(9);
+  vw_setup(500);
+  vw_rx_start();
+
+}
+
+void loop() {
+
+  comando = Serial.read();
+
+  //TRANSMISSOR
+  if (comando == '1') {
+
+    valor = 1;
+    itoa (valor, Valor_CharMsg, 10); //Converte valor para envio
+    delay(1000);
+    vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+    vw_wait_tx(); //Aguarda envio dos dados
+    delay(1000);
+
+    //RECEPETOR
+    //Estabilização
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen))
+    {
+      int i;
+      for (i = 0; i < buflen; i++)
+      {
+        recebido_RF_char[i] = char(buf[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buflen] = '\0';
+      valRec = atof(recebido_RF_char);
+
+    }
+
+    Serial.print("Estabilizacao de sinal... ");
+
+
+    delay(2000);
+
+
+    uint8_t buf2[VW_MAX_MESSAGE_LEN];
+    uint8_t buf2len = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf2, &buf2len))
+    {
+      int i;
+      for (i = 0; i < buf2len; i++)
+      {
+        recebido_RF_char[i] = char(buf2[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf2len] = '\0';
+      valRec = atof(recebido_RF_char);
+
+    }
+
+    Serial.print("Temperatura: ");
+    Serial.println(valRec);
+  }
+
+
+
+  //TRANSMISSOR
+  if (comando == '2') {
+
+    valor = 2;
+    itoa (valor, Valor_CharMsg, 10); //Converte valor para envio
+    delay(1000);
+    vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+    vw_wait_tx(); //Aguarda envio dos dados
+    delay(1000);
+
+    //RECEPETOR
+    //Estabilização
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen))
+    {
+      int i;
+      for (i = 0; i < buflen; i++)
+      {
+        recebido_RF_char[i] = char(buf[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buflen] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+
+    Serial.print("Estabilizacao de sinal...");
+
+
+    delay(2000);
+
+    uint8_t buf1[VW_MAX_MESSAGE_LEN];
+    uint8_t buf1len = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf1, &buf1len))
+    {
+      int i;
+      for (i = 0; i < buf1len; i++)
+      {
+        recebido_RF_char[i] = char(buf1[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf1len] = '\0';
+      valRec = atof(recebido_RF_char);
+
+    }
+
+    Serial.print(". ");
+
+
+    delay(2000);
+
+    
+    uint8_t buf2[VW_MAX_MESSAGE_LEN];
+    uint8_t buf2len = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf2, &buf2len))
+    {
+      int i;
+      for (i = 0; i < buf2len; i++)
+      {
+        recebido_RF_char[i] = char(buf2[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf2len] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+
+    Serial.print("Umidade: ");
+    Serial.println(valRec);
+
+  }
+
+  //TRANSMISSOR
+  if (comando == '3') {
+
+    valor = 3;
+    itoa (valor, Valor_CharMsg, 10); //Converte valor para envio
+    delay(1000);
+    vw_send((uint8_t *)Valor_CharMsg, strlen(Valor_CharMsg)); //Envio dos dados
+    vw_wait_tx(); //Aguarda envio dos dados
+    delay(1000);
+
+    //RECEPETOR
+    //Estabilização
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen))
+    {
+      int i;
+      for (i = 0; i < buflen; i++)
+      {
+        recebido_RF_char[i] = char(buf[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buflen] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+
+    Serial.print("Estabilizacao de sinal...");
+
+
+    delay(2000);
+    
+    uint8_t buff1[VW_MAX_MESSAGE_LEN];
+    uint8_t buff1len = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buff1, &buff1len))
+    {
+      int i;
+      for (i = 0; i < buff1len; i++)
+      {
+        recebido_RF_char[i] = char(buff1[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buff1len] = '\0';
+      valRec = atof(recebido_RF_char);
+
+    }
+
+    Serial.print(".");
+
+
+    delay(2000);
+    
+    uint8_t buff2[VW_MAX_MESSAGE_LEN];
+    uint8_t buff2len = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buff2, &buff2len))
+    {
+      int i;
+      for (i = 0; i < buff2len; i++)
+      {
+        recebido_RF_char[i] = char(buff2[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buff2len] = '\0';
+      valRec = atof(recebido_RF_char);
+
+    }
+
+    Serial.println(".");
+
+
+    delay(3000);
+
+
+
+    //SINAL PLACA 1
+    uint8_t buf1[VW_MAX_MESSAGE_LEN];
+    uint8_t buf1len = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf1, &buf1len))
+    {
+      int i;
+      for (i = 0; i < buf1len; i++)
+      {
+        recebido_RF_char[i] = char(buf1[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf1len] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+
+    Serial.print(" P1  ");
+    Serial.print(valRec);
+    Serial.print(" Volts");
+
+
+    delay(2000);
+
+
+    //SINAL PLACA 2
+    uint8_t buf2[VW_MAX_MESSAGE_LEN];
+    uint8_t buf2len = VW_MAX_MESSAGE_LEN;
+
+    delay(1000);
+
+    if (vw_get_message(buf2, &buf2len))
+    {
+      int i;
+      for (i = 0; i < buf2len; i++)
+      {
+        recebido_RF_char[i] = char(buf2[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf2len] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+    Serial.print(" P2  ");
+    Serial.print(valRec);
+    Serial.print(" Volts");
+
+
+    delay(2000);
+
+
+    //SINAL PLACA 3
+    uint8_t buf3[VW_MAX_MESSAGE_LEN];
+    uint8_t buf3len = VW_MAX_MESSAGE_LEN;
+
+    delay(1000);
+
+    if (vw_get_message(buf3, &buf3len))
+    {
+      int i;
+      for (i = 0; i < buf3len; i++)
+      {
+        recebido_RF_char[i] = char(buf3[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf3len] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+    Serial.print(" P3  ");
+    Serial.print(valRec);
+    Serial.print(" Volts");
+
+
+    delay(2000);
+
+    //SINAL PLACA 4
+    uint8_t buf4[VW_MAX_MESSAGE_LEN];
+    uint8_t buf4len = VW_MAX_MESSAGE_LEN;
+
+    delay(1000);
+
+    if (vw_get_message(buf4, &buf4len))
+    {
+      int i;
+      for (i = 0; i < buf4len; i++)
+      {
+        recebido_RF_char[i] = char(buf4[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf4len] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+    Serial.print(" P4  ");
+    Serial.print(valRec);
+    Serial.print(" Volts");
+
+
+    delay(2000);
+
+    //SINAL PLACA 5
+    uint8_t buf5[VW_MAX_MESSAGE_LEN];
+    uint8_t buf5len = VW_MAX_MESSAGE_LEN;
+
+    delay(1000);
+
+    if (vw_get_message(buf5, &buf5len))
+    {
+      int i;
+      for (i = 0; i < buf5len; i++)
+      {
+        recebido_RF_char[i] = char(buf5[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf5len] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+    Serial.print(" P5  ");
+    Serial.print(valRec);
+    Serial.print(" Volts");
+
+    delay(2000);
+
+    //SINAL PLACA 6
+    uint8_t buf6[VW_MAX_MESSAGE_LEN];
+    uint8_t buf6len = VW_MAX_MESSAGE_LEN;
+
+    delay(1000);
+
+    if (vw_get_message(buf6, &buf6len))
+    {
+      int i;
+      for (i = 0; i < buf6len; i++)
+      {
+        recebido_RF_char[i] = char(buf6[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[buf6len] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+    Serial.print(" P6  ");
+    Serial.print(valRec);
+    Serial.print(" Volts");
+
+    delay(2000);
+
+    //SINAL ÂNGULO ALFA
+    uint8_t bufA[VW_MAX_MESSAGE_LEN];
+    uint8_t bufAlen = VW_MAX_MESSAGE_LEN;
+
+    delay(1000);
+
+    if (vw_get_message(bufA, &bufAlen))
+    {
+      int i;
+      for (i = 0; i < bufAlen; i++)
+      {
+        recebido_RF_char[i] = char(bufA[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[bufAlen] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+    Serial.print(" Alfa:  ");
+    Serial.print(valRec);
+    Serial.print(" graus");
+
+    delay(2000);
+
+    //SINAL ÂNGULO BETA
+    uint8_t bufB[VW_MAX_MESSAGE_LEN];
+    uint8_t bufBlen = VW_MAX_MESSAGE_LEN;
+
+    delay(1000);
+
+    if (vw_get_message(bufB, &bufBlen))
+    {
+      int i;
+      for (i = 0; i < bufBlen; i++)
+      {
+        recebido_RF_char[i] = char(bufB[i]); //Armazena os caracteres recebidos
+      }
+      recebido_RF_char[bufBlen] = '\0';
+      valRec = atof(recebido_RF_char);
+    }
+    Serial.print(" Beta:  ");
+    Serial.print(valRec);
+    Serial.println(" graus");
+
+  }
+}
